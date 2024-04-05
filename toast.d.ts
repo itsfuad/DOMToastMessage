b3c9175092428c78e114f518f3972a979130afe2
Fuad Hasan <fuad.cs22@gmail.com>
2024-04-05 19:42:14 +0600

    Update package version to 1.0.7, delete toast.d.ts and toast.js files

--- toast.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Shows a popup message for 1 second
- * @param {string} text Text to show in the popup
- * @param {number} timeout Time in milliseconds to show the popup
- * @param {string} backgroundColor Background color of the popup
- * @param {string} color Text color of the popup
- */
-export declare function showToastMessage(text: string, timeout?: number, backgroundColor?: string, color?: string): void;
